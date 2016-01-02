14a21cf4972d0bcf7569be7b4d08463fc6d24f8e
Robin <robimeht@umich.edu>
2016-01-02 00:32:06 -0500

    can create many journals now!

---
+++ list/singleJournalDetailCell.swift
@@ -0,0 +1,13 @@
+//
+//  singleJournalDetailCell.swift
+//  list
+//
+//  Created by Robin Mehta on 1/1/16.
+//  Copyright © 2016 robin. All rights reserved.
+//
+
+import Cocoa
+
+class singleJournalDetailCell: UITableViewCell {
+
+}
